df2a5e6354df42bc5a18eea0e1df107e60d8d9e7
Deimer Navarro <deimer.navarro@esinergia.co>
2024-05-06 17:47:26 -0500

    added tamagui library

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { TamaguiProvider, Text, View } from 'tamagui';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TamaguiProvider>
+      <View style={styles.container}>
+        <Text>WEGOAL app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </TamaguiProvider>
   );
 }
 
